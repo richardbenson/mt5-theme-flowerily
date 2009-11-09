cca9ee60798d4b9a832b635b9117a2a8f5e8fcf2
Richard Benson <me@richardbenson.co.uk>
2009-11-09 16:14:52 +0000

    Move styles to style_library and remove mt-static dir

--- mt-static/themes/flowerily/js/auth.js
+++
@@ -1,9 +0,0 @@
-function showAuth(authType) {
-	var authTypes = RB.Dom.getChildren('auth-types');
-	for (i=0; i < authTypes.length; i++) {
-		RB.Dom.setStyle(authTypes[i], 'display','none');
-		RB.Dom.removeClass('auth-select-' + authTypes[i].id.replace('auth-form-', ''), 'selected');
-	}
-	RB.Dom.setStyle('auth-form-' + authType, 'display','');
-	RB.Dom.addClass('auth-select-' + authType, 'selected');
-}
